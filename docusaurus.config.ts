d5242b9f00b175a59101c07ed7d47f000240dfef
YvainVDP <yvain.vdp@gmail.com>
2026-04-05 14:40:46 +0200

    little change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -87,7 +87,7 @@ const config: Config = {
   ],
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/logo.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
